6958bb27ac1993ac86f890ed3aa31f22a205e9f9
DannyyDev <42282680+DannyyDev@users.noreply.github.com>
2020-09-30 00:39:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login("BOT TOKEN");
+const config = require('./config.json');
+const prefix = config.PREFIX;
+client.on('message', async message => {
+var args = message.content.slice(prefix.length).split(/ +/g);
+var command = args.shift().toLowerCase();
+if (command == "say") {
+if (!args[0]) return message.channel.send("Ingresa algún mensaje a ser enviado. `!say <mensaje>`")
+message.channel.send(args.join(" "));
+};
+});
+client.login(config.TOKEN);
